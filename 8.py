de9c512239e5c775dc1bbb9cdb7af89cf5134a68
Aikumar123680 <b_aikumar@kbtu.kz>
2025-01-23 17:24:49 +0500

    Add

---
+++ 8.py
@@ -0,0 +1,8 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
